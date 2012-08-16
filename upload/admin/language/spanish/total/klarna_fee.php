7b462017af6fa3f4c9b855a21cbdae73491ccd4d
rowend <rowend07@gmail.com>
2012-08-16 17:47:44 -0500

    admin-total-folder

---
+++ upload/admin/language/spanish/total/klarna_fee.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'Cuota de Klarna';
+
+// Text
+$_['text_total']       = 'Totales del pedido';
+$_['text_success']     = 'Éxito: has modificado el total de la cuota de Klarna!';
+
+// Entry
+$_['entry_total']      = 'Total del Pedido:';
+$_['entry_fee']        = 'Cuota:';
+$_['entry_tax_class']  = 'Tipo de Impuesto:';
+$_['entry_status']     = 'Estado:';
+$_['entry_sort_order'] = 'Orden de Aparición:';
+
+// Error
+$_['error_permission'] = 'Cuidado: no tienes permisos para modificar el total de la Cuota de Klarna!';
+?>
