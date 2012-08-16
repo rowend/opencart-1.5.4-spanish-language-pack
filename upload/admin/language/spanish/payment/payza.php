a1595956516f0f4fb67dcf124f3debe9d0092ba9
rowend <rowend07@gmail.com>
2012-08-16 16:14:28 -0500

    admin-payments-payza

--- upload/admin/language/spanish/payment/payza.php
+++ upload/admin/language/spanish/payment/payza.php
@@ -2,22 +2,22 @@
 // Heading
 $_['heading_title']      = 'Payza';
 
-// Text 
-$_['text_payment']       = 'Payment';
-$_['text_success']       = 'Success: You have modified Payza account details!';
-      
+// Text
+$_['text_payment']       = 'Pago';
+$_['text_success']       = 'Éxito: has modificado los detalles de tu cuenta de Payza!';
+
 // Entry
-$_['entry_merchant']     = 'Merchant ID:';
-$_['entry_security']     = 'Security Code:';
-$_['entry_callback']     = 'Alert URL:<br /><span class="help">This has to be set in the Payza control panel. You will also need to check the "IPN Status" to enabled.</span>';
-$_['entry_total']        = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
-$_['entry_order_status'] = 'Order Status:';
-$_['entry_geo_zone']     = 'Geo Zone:';
-$_['entry_status']       = 'Status:';
-$_['entry_sort_order']   = 'Sort Order:';
+$_['entry_merchant']     = 'ID de comerciante:';
+$_['entry_security']     = 'Código de seguridad:';
+$_['entry_callback']     = 'Alert URL:<br /><span class="help">Esto tiene que ser puesto en el panel de control Payza. Tu tendras que checar el "IPN Status" para habilitarlo.</span>';
+$_['entry_total']        = 'Total:<br /><span class="help">El total de la compra de la orden debe alcanzar lo establecido antes que este metodo de pago sea activado.</span>';
+$_['entry_order_status'] = 'Orden de Aparición:';
+$_['entry_geo_zone']     = 'Zona Geográfica:';
+$_['entry_status']       = 'Estado:';
+$_['entry_sort_order']   = 'Orden de aparición:';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment Payza!';
-$_['error_merchant']     = 'Merchant ID Required!';
-$_['error_security']     = 'Security Code Required!';
+$_['error_permission']   = 'Cuidado: No tienes persmisos para modificar pagos Payza!';
+$_['error_merchant']     = 'ID de comerciante obligatorio!';
+$_['error_security']     = 'Código de seguridad obligatorio!';
 ?>
