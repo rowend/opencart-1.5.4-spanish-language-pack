d57ff7eb3061ab9b84de60833957a6927baa3ec3
rowend <rowend07@gmail.com>
2012-08-16 17:00:26 -0500

    admin-shipping-auspost

--- upload/admin/language/spanish/shipping/auspost.php
+++ upload/admin/language/spanish/shipping/auspost.php
@@ -3,21 +3,21 @@
 $_['heading_title']      = 'Australia Post';
 
 // Text
-$_['text_shipping']      = 'Shipping';
-$_['text_success']       = 'Success: You have modified Australia Post shipping!';
+$_['text_shipping']      = 'Envío';
+$_['text_success']       = 'Éxito: Has modificado los envíos Australia!';
 
 // Entry
-$_['entry_postcode']     = 'Post Code:';
-$_['entry_express']      = 'Express Postage:';
-$_['entry_standard']     = 'Standard Postage:';
-$_['entry_display_time'] = 'Display Delivery Time:<br /><span class="help">Do you want to display the shipping time? (e.g. Ships within 3 to 5 days)</span>';
-$_['entry_weight_class'] = 'Weight Class:<span class="help">Set to grams.</span>';
-$_['entry_tax_class']    = 'Tax Class:';
-$_['entry_geo_zone']     = 'Geo Zone:';
-$_['entry_status']       = 'Status:';
-$_['entry_sort_order']   = 'Sort Order:';
+$_['entry_postcode']     = 'Código Postal:';
+$_['entry_express']      = 'Envío expreso:';
+$_['entry_standard']     = 'Envío estandar:';
+$_['entry_display_time'] = 'Mostrar la hora de entrega:<br /><span class="help">¿Quieres mostrar el tiempo de entrega? (ej. Envíos dentro de 3 a 5 días)</span>';
+$_['entry_weight_class'] = 'Tipo de Peso:<span class="help">Establecido en gramos.</span>';
+$_['entry_tax_class']    = 'Tipo de Impuesto:';
+$_['entry_geo_zone']     = 'Zona Geográfica:';
+$_['entry_status']       = 'Estado:';
+$_['entry_sort_order']   = 'Orden de Aparición:';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify Australia Post shipping!';
-$_['error_postcode']     = 'Post Code must be 4 digits!';
+$_['error_permission']   = 'Cuidado: No tienes permisos para modificar el servicio de envíos Australia Post shipping!';
+$_['error_postcode']     = 'El código postal debe ser de 4 digítos!';
 ?>
