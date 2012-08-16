6d3b1503506212e7a0daad5ae165b894db0db1b4
rowend <rowend07@gmail.com>
2012-08-16 16:21:46 -0500

    admin-report-customer-onlie

--- upload/admin/language/spanish/report/customer_online.php
+++ upload/admin/language/spanish/report/customer_online.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']     = 'Customers Online Report';
+$_['heading_title']     = 'Reporte de Clientes en Linea';
 
-// Text 
-$_['text_guest']        = 'Guest';
- 
+// Text
+$_['text_guest']        = 'Invitado';
+
 // Column
 $_['column_ip']         = 'IP';
-$_['column_customer']   = 'Customer';
-$_['column_url']        = 'Last Page Visited';
-$_['column_referer']    = 'Referer';
-$_['column_date_added'] = 'Last Click';
-$_['column_action']     = 'Action';
+$_['column_customer']   = 'Cliente';
+$_['column_url']        = 'Ultima página visitada';
+$_['column_referer']    = 'Remitir';
+$_['column_date_added'] = 'Ultimo Click';
+$_['column_action']     = 'Acción';
 ?>
